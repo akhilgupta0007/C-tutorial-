c1bc1066aca0d7863c594e7d6a513bd93bf0bff9
akhilgupta0007 <akhilrajgupta1008@gmail.com>
2026-05-03 15:13:10 +0530

    to find simple interest

---
+++ second code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float p,r,t;
+    cout<<"enter p,r,t";
+    //p=principle
+    //r=rate
+    //t=time
+    cin>>p>>r>>t;
+    cout<<"simple interest is"<<(p*r*t)/100;
+}
